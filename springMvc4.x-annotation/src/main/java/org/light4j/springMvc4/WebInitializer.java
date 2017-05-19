d09fd5517dfe46378b93ebb17e15bf8b4e1209eb
longjiazuo <812198178@qq.com>
2017-05-19 13:49:32 +0800

    增加常用注解章节的示例代码

---
+++ springMvc4.x-annotation/src/main/java/org/light4j/springMvc4/WebInitializer.java
@@ -0,0 +1,26 @@
+package org.light4j.springMvc4;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebInitializer implements WebApplicationInitializer {// ①
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(MyMvcConfig.class);
+		ctx.setServletContext(servletContext); // ②
+
+		Dynamic servlet = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(ctx)); // 3
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+		servlet.setAsyncSupported(true);// ①
+	}
+}
