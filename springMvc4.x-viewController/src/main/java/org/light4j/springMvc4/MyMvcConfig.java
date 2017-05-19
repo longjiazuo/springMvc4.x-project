8ea82dad17e81735ca073aa47e9413f5d1a92922
longjiazuo <812198178@qq.com>
2017-05-19 16:09:29 +0800

    增加快捷的ViewController示例代码的配置

---
+++ springMvc4.x-viewController/src/main/java/org/light4j/springMvc4/MyMvcConfig.java
@@ -0,0 +1,49 @@
+package org.light4j.springMvc4;
+
+import org.light4j.springMvc4.interceptor.DemoInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc// ①
+@ComponentScan("org.light4j.springMvc4")
+public class MyMvcConfig extends WebMvcConfigurerAdapter {// ②
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+    
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");// ③
+	}
+    
+    @Bean
+	// ①
+	public DemoInterceptor demoInterceptor() {
+		return new DemoInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {// ②
+		registry.addInterceptor(demoInterceptor());
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("/index");
+	}
+}
---
+++ springMvc4.x-viewController/src/main/java/org/light4j/springMvc4/interceptor/DemoInterceptor.java
@@ -0,0 +1,29 @@
+package org.light4j.springMvc4.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class DemoInterceptor extends HandlerInterceptorAdapter {//①
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, //②
+			HttpServletResponse response, Object handler) throws Exception {
+		long startTime = System.currentTimeMillis();
+		request.setAttribute("startTime", startTime);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, //③
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		long startTime = (Long) request.getAttribute("startTime");
+		request.removeAttribute("startTime");
+		long endTime = System.currentTimeMillis();
+		System.out.println("本次请求处理时间为:" + new Long(endTime - startTime)+"ms");
+		request.setAttribute("handlingTime", endTime - startTime);
+	}
+}
